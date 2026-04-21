47c6789273bbabcb324caa2ec7191bc942f0ec3b
shaikhutdinova06-gif <shaikhutdinova06-gif@users.noreply.github.com>
2026-04-21 11:30:42 +0500

    fix: add fonts.googleapis.com and fonts.gstatic.com to CSP for Google Fonts

--- main.py
+++ main.py
@@ -22,7 +22,7 @@ async def add_security_headers(request, call_next):
     response.headers["Content-Security-Policy"] = (
         "default-src 'self'; "
         "script-src 'self' 'unsafe-inline' https://unpkg.com https://cdn.jsdelivr.net https://cdn.plot.ly; "
-        "style-src 'self' 'unsafe-inline' https://unpkg.com; "
+        "style-src 'self' 'unsafe-inline' https://unpkg.com https://fonts.googleapis.com https://fonts.gstatic.com; "
         "img-src 'self' data: https:; "
         "connect-src 'self' https:;"
     )
